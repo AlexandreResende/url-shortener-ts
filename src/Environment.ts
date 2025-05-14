35f44b7d9e1592de1c7767409760216c88f268b4
AlexandreResende <alexandre.amres@gmail.com>
2025-05-14 10:15:43 -0300

    chore: Add dotenv to project

---
+++ src/Environment.ts
@@ -0,0 +1,9 @@
+import 'dotenv/config';
+
+const ENVIRONMENT = {
+  SERVER: {
+    PORT: process.env.PORT || 3000,
+  }
+};
+
+export default ENVIRONMENT;
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import express, { Request, Response } from "express";
 
+import ENVIRONMENT from "./Environment";
+
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`Server running on port: ${PORT}`);
+app.listen(ENVIRONMENT.SERVER.PORT, () => {
+  console.log(`Server running on port: ${ENVIRONMENT.SERVER.PORT}`);
 });
