50a83e2a3d5600211975a55c217d373b29689fad
AlexandreResende <alexandre.amres@gmail.com>
2025-05-19 23:09:56 -0300

    chore: Add assertion on required environment variables

--- src/Environment.ts
+++ src/Environment.ts
@@ -1,8 +1,19 @@
 import 'dotenv/config';
 
+const requiredEnvVar = (environmentVariable: any, variableName: string): any=> {
+  if (environmentVariable === undefined) {
+    throw new Error(`Missing environment variable: ${variableName}`);
+  }
+
+  return environmentVariable;
+}
+
 const ENVIRONMENT = {
   SERVER: {
     PORT: process.env.PORT || 3000,
+  },
+  CAPACITY: {
+    AMOUNT_OF_REQUEST_PER_MONTH:requiredEnvVar(process.env.AMOUNT_OF_REQUEST_PER_MONTH, 'AMOUNT_OF_REQUEST_PER_MONTH'),
   }
 };
 
