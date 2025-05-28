0e58221f85173f35a41a1ee50b57669ee15c1ae3
AlexandreResende <alexandre.amres@gmail.com>
2025-05-28 18:10:36 -0300

    chore: Add nyc for coverage report

--- src/routers/HealthCheckRouter.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-import HealthCheckController from '../controllers/HealthCheckController';
-
-const healthCheckRouter = Router();
-
-healthCheckRouter.get('/healthz', new HealthCheckController().handleRequest);
-
-export default healthCheckRouter;
