c88ff6e2e8c02b7995d2591809d7c59a9aa0cdb6
AlexandreResende <alexandre.amres@gmail.com>
2025-05-30 22:34:24 -0300

    tests: Fix broken tests

--- tests/unit/commands/redirectURLCommand.spec.ts
+++ tests/unit/commands/redirectURLCommand.spec.ts
@@ -1,33 +1,37 @@
 import { faker } from "@faker-js/faker";
+import { expect } from "chai";
+import Sinon from "sinon";
+
 import HashService from "../../../src/services/HashService";
 import RedirectURLCommand from "../../../src/commands/RedirectURLCommand";
-import db from "../../../src/localDB";
-import { expect } from "chai";
-
+import URLRepository from "../../../src/repositories/Redis/URLRepository";
 
 describe('Unit test', function() {
   describe('RedirectURLCommand', function() {
     const hashService = new HashService();
 
+    this.afterEach(() => {
+      Sinon.restore();
+    });
+
     it('returns an URL redirect the user', async function() {
       const url = faker.internet.url();
       const hashedURL = hashService.hash(url);
 
-      db.set(hashedURL, url);
+      const getKey = Sinon.stub().resolves(url);
 
-      const command = new RedirectURLCommand();
+      const command = new RedirectURLCommand({ getKey } as unknown as URLRepository);
       const result = await command.execute({ urlId: hashedURL });
 
       expect(result.url).to.be.equal(url);
-
-      db.delete(hashedURL);
     });
 
     it('returns undefined when hash does not exist', async function() {
       const url = faker.internet.url();
       const hashedURL = hashService.hash(url);
+      const getKey = Sinon.stub().resolves(null);
 
-      const command = new RedirectURLCommand();
+      const command = new RedirectURLCommand({ getKey } as unknown as URLRepository);
       const result = await command.execute({ urlId: hashedURL });
 
       expect(result.url).to.be.equal(undefined);
