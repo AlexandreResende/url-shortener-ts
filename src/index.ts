1ce8b49e8dd866ed0c835de3368749e35625d0fc
AlexandreResende <alexandre.amres@gmail.com>
2026-02-26 23:09:06 -0300

    feature: add rate-limiter to project

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
 import 'reflect-metadata';
 import './di'
 import express from "express";
+import { rateLimit } from "express-rate-limit";
 
 import ENVIRONMENT from "./Environment";
 import Routers from "./routers/Routers";
 
+const limiter = rateLimit({
+  limit: 5,
+  windowMs: 1000 * 60 * 1,
+  message: "Too many requests, please try again later."
+});
+
 const app = express();
 
 app.use(express.json());
+app.use(limiter);
 
 app.use(Routers);
 
